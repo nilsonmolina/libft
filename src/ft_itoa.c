f53343fcfb88a898d8da72425d48324a0dfc13c2
Nilson Molina <nmolina@student.42.us.org>
2018-05-25 14:31:06 -0700

    changed ft_itoa - was causing illegal hdw instructions in fractol and fdf under heavy usage.

--- src/ft_itoa.c
+++ src/ft_itoa.c
@@ -3,44 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmolina <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: nmolina <nmolina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/08 16:56:04 by nmolina           #+#    #+#             */
-/*   Updated: 2017/12/08 17:00:55 by nmolina          ###   ########.fr       */
+/*   Updated: 2018/05/25 14:29:41 by nmolina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	get_str_len(int n)
+char	*ft_itoa(int n)
 {
-	size_t		i;
+	char	*s;
+	long	nbr;
+	int		len;
 
-	i = 1;
-	while (n /= 10)
-		i++;
-	return (i);
-}
-
-char			*ft_itoa(int n)
-{
-	char			*str;
-	size_t			str_len;
-	unsigned int	n_cpy;
-
-	str_len = get_str_len(n);
-	n_cpy = n;
-	if (n < 0)
+	len = 1;
+	n < 0 ? ++len : 0;
+	nbr = n < 0 ? -(long)n : n;
+	while (nbr > 9)
 	{
-		n_cpy = -n;
-		str_len++;
+		nbr /= 10;
+		++len;
 	}
-	if (!(str = ft_strnew(str_len)))
-		return (NULL);
-	str[--str_len] = n_cpy % 10 + '0';
-	while (n_cpy /= 10)
-		str[--str_len] = n_cpy % 10 + '0';
-	if (n < 0)
-		*(str + 0) = '-';
-	return (str);
+	s = (char*)malloc(sizeof(char) * (len + 1));
+	s[len] = '\0';
+	n < 0 ? *s = '-' : 0;
+	nbr = n < 0 ? -(long)n : n;
+	while (nbr > 9)
+	{
+		s[--len] = (nbr % 10) + '0';
+		nbr /= 10;
+	}
+	s[--len] = nbr + '0';
+	return (s);
 }
